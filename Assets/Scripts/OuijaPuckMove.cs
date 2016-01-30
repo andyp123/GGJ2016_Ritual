fe2e6872873141770f740b1fc3e371954d396268
Andrew Palmer <andyp.123@gmail.com>
2016-01-30 23:34:50 +0900

    Hack in simple puck rotation

--- Assets/Scripts/OuijaPuckMove.cs
+++ Assets/Scripts/OuijaPuckMove.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class OuijaPuckMove : MonoBehaviour
 {
   public float moveSpeed = 0.05f;
+  public float maxRotation = 30.0f;
+  public float maxMoveX = 20.0f;
+  public float maxMoveY = 13.0f;
 
 	void Update ()
   {
@@ -12,5 +15,8 @@ public class OuijaPuckMove : MonoBehaviour
 	  Vector3 moveDir = new Vector3(x, y, 0.0f).normalized;
 
     transform.Translate(moveDir * moveSpeed * Time.deltaTime);
+
+    float rotAngle = transform.localPosition.x / maxMoveX * maxRotation;
+    transform.localRotation = Quaternion.Euler(270.0f, 0.0f, rotAngle);
 	}
 }
