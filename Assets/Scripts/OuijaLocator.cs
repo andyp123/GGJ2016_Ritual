3cceaa8b72d20ec964e66674156f67c1b270d5f2
Andrew Palmer <andyp.123@gmail.com>
2016-01-30 14:06:19 +0900

    Add Ouija Board assets

---
+++ Assets/Scripts/OuijaLocator.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OuijaLocator : MonoBehaviour
+{
+
+  public GameObject ouijaPuck;
+  public float distanceThreshold = 0.02f;
+  public float puckRestThreshold = 0.75f;
+  public float puckWobbleThreshold = 0.001f;
+
+  private Transform nearestLocator;
+  private Transform previousLocator;
+  private Vector3 puckLastPos;
+  private float puckRestTime = 0.0f;
+
+	void Start ()
+  {
+    puckLastPos = ouijaPuck.transform.position;
+
+    // foreach (Transform child in transform)
+    // {
+    //   Debug.Log ( child.name );
+    // }
+	}
+	
+	void Update ()
+  {
+    float wobbleDistance = (ouijaPuck.transform.position - puckLastPos).magnitude;
+    puckRestTime = (wobbleDistance <= puckWobbleThreshold) ? puckRestTime += Time.deltaTime : 0.0f;
+
+    if (puckRestTime >= puckRestThreshold)
+    {
+      UpdateNearestLocator();
+      if (nearestLocator != null && nearestLocator != previousLocator)
+      {
+        Debug.Log(nearestLocator.name);
+      }
+    }
+
+    puckLastPos = ouijaPuck.transform.position;
+	}
+
+  void UpdateNearestLocator ()
+  {
+    Transform currentNearest = transform;
+    Vector3 puckPos = ouijaPuck.transform.position;
+    float nearestDistance = 9999999.0f;
+
+    foreach (Transform child in transform)
+    {
+      float distance = (child.position - puckPos).magnitude;
+      if (distance < nearestDistance)
+      {
+        nearestDistance = distance;
+        currentNearest = child;
+      }
+    } 
+
+    if (nearestDistance < distanceThreshold)
+    {
+      previousLocator = nearestLocator;
+      nearestLocator = currentNearest;
+    }
+    else
+    {
+      nearestLocator = null;
+    } 
+  }
+}
---
+++ Assets/Scripts/OuijaPuckMove.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OuijaPuckMove : MonoBehaviour
+{
+  public float moveSpeed = 0.05f;
+
+	void Update ()
+  {
+    float x = Input.GetAxis("Horizontal");
+    float y = -Input.GetAxis("Vertical");
+	  Vector3 moveDir = new Vector3(x, y, 0.0f).normalized;
+
+    transform.Translate(moveDir * moveSpeed * Time.deltaTime);
+	}
+}
